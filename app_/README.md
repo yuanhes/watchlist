883b28d593e06ad8118a17983fcc678521507c4f
Franz Song <justsyh@gmail.com>
2025-08-08 04:43:07 +0800

    try to split the app

---
+++ app_/README.md
@@ -0,0 +1,7 @@
+模块        |作用
+-           |-
+__init__.py |包构造文件，创建程序实例
+views.py    |视图函数
+errors.py   |错误处理函数
+models.py   |模型类
+commands.py |命令函数
---
+++ app_/__init__.py
@@ -0,0 +1,31 @@
+mport os
+import sys
+
+from flask_login import LoginManager, login_required, logout_user, login_user, current_user
+
+from flask import Flask, url_for, render_template, request, redirect, flash
+from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+login_manager = LoginManager(app)  # 实例化扩展类
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的>用户
+    return user  # 返回用户对象
+
+
+
